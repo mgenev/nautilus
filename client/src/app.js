caa7961acae47e265e6ee25ca9c3daf99c33b2d7
Martin Genev <mgenev@gmail.com>
2015-04-29 08:56:16 -0700

    adding aurelia skeleton

---
+++ client/src/app.js
@@ -0,0 +1,19 @@
+import {inject} from 'aurelia-framework';
+import {Router} from 'aurelia-router';
+import 'bootstrap';
+import 'bootstrap/css/bootstrap.css!';
+
+@inject(Router)
+export class App {
+  constructor(router) {
+    this.router = router;
+    this.router.configure(config => {
+      config.title = 'Aurelia';
+      config.map([
+        { route: ['','welcome'],  moduleId: './welcome',      nav: true, title:'Welcome' },
+        { route: 'flickr',        moduleId: './flickr',       nav: true },
+        { route: 'child-router',  moduleId: './child-router', nav: true, title:'Child Router' }
+      ]);
+    });
+  }
+}
