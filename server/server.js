130e491d90b92122282498e616ce088072e5632d
Martin Genev <mgenev@gmail.com>
2015-05-22 15:13:08 -0700

    replaced server with express-restify-mongoose

---
+++ server/server.js
@@ -0,0 +1,34 @@
+var http = require('http');
+var express = require('express');
+var morgan = require('morgan');
+var path = require('path');
+var restify = require('express-restify-mongoose');
+var bodyParser = require('body-parser'),
+	  methodOverride = require('method-override');
+var mongoose = require('mongoose');
+mongoose.connect('mongodb://localhost/nautilus');
+var db = mongoose.connection;
+
+var ToDoSchema = new mongoose.Schema({
+	text: { type: String, required: true },
+	done: { type: Boolean, default: false }
+});
+var ToDoModel = mongoose.model('todo', ToDoSchema);
+
+var app = express();
+app.set('port', process.env.PORT || 3000);
+app.use(morgan('dev'));
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(bodyParser.json());
+app.use(methodOverride('X-HTTP-Method-Override'));
+restify.serve(app, ToDoModel, {
+	//exclude: 'text,done'
+});
+// app.use(express.static(path.join(__dirname, 'public')));
+// app.use(function (req, res) {
+	// res.sendFile(path.join(__dirname, 'public/index.html'));
+// });
+
+http.createServer(app).listen(app.get('port'), function () {
+	console.log('Express server listening on port ' + app.get('port'));
+});
