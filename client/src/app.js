4206832ac07386a0fc0e28e805ae60d6de9f90eb
mgenev <mgenev@gmail.com>
2015-05-30 19:01:22 -0700

    clean up

--- client/src/app.js
+++ client/src/app.js
@@ -8,11 +8,10 @@ export class App {
       { route: ['','welcome'],   moduleId: './routes/welcome',      nav: true, title:'Welcome' },
       { route: 'geo-demo',       moduleId: './routes/geo-demo',     nav: true, title: 'Geo Demo' },
       { route: 'posts',          moduleId: './routes/posts/index',     name: 'posts', nav: true, title: 'Posts' },
-      { route: 'posts/:id',      moduleId: './routes/posts/post',      name: 'postById', title: this.dynamicTitle },
+      { route: 'posts/:id',      moduleId: './routes/posts/post',      name: 'postById'},
       { route: 'posts/new',      moduleId: './routes/posts/new',           nav: true, title: 'New Post' }
     ]);
 
     this.router = router;
-    this.router.dynamicTitle = 'WTF!';
   }
 }
