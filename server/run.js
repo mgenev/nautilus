382d4cfea52102740ad6b4af5457959e4330c34d
mgenev <mgenev@gmail.com>
2015-05-31 11:48:53 -0700

    clean up

--- server/run.js
+++ server/run.js
@@ -1,6 +1,7 @@
-require('babel/register');
+require('babel/register')({
+  optional: ['bluebirdCoroutines']
+});
 var app = require('./server');
-
 
 // auto init if this app is not being initialized by another module (i.e. using require('./app').init();)
 if (!module.parent) {
