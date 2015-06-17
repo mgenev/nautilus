fd618bf1689319eb2b595f6312e3ada35fe9233a
Martin Genev <mgenev@gmail.com>
2015-06-17 09:57:21 -0700

    create service for vendor

--- client/src/routes/vendors/service-add.js
+++ client/src/routes/vendors/service-add.js
@@ -1,11 +1,12 @@
-import {customElement, bindable} from 'aurelia-framework';
+import {customElement, bindable, inject} from 'aurelia-framework';
 import {HttpClient} from 'aurelia-http-client';
 import {Config} from 'services/config';
 
 @customElement('service-add')
+@inject(HttpClient, Config)
 export class SayHello {
   @bindable vendor = null;
-  endpoint = 'services';
+  endPoint = 'services';
   service = {};
 
   constructor(http, config){
@@ -16,8 +17,10 @@ export class SayHello {
   async createService() {
     try {
       this.service.vendor = this.vendor._id;
-      let newService = await this.http.post(`${this.config.server.url}${this.endPoint}`, this.vendor);
-      this.router.navigateToRoute('serviceById', {id: newService.content._id});
+      let newService = await this.http.post(`${this.config.server.url}${this.endPoint}`, this.service);
+      // TODO add new service to the parent's list
+      // this.router.navigateToRoute('serviceById', {id: newService.content._id});
+
     } catch (err) {
       // TODO flash a global error message
       console.log('error creating service: ', err);
--- client/src/routes/vendors/vendor.js
+++ client/src/routes/vendors/vendor.js
@@ -5,8 +5,6 @@ import {Config} from 'services/config';
 @inject(HttpClient, Config)
 export class Posts {
   heading = 'Single Vendor';
-  endPoint = 'vendors';
-  services = ['service1', 'service2'];
 
   constructor(http, config){
     this.http = http;
@@ -15,8 +13,10 @@ export class Posts {
 
   async activate(params, routeConfig) {
     try {
-      let vendor = await this.http.get(`${this.config.server.url}${this.endPoint}/${params.id}`);
+      let vendor = await this.http.get(`${this.config.server.url}vendors/${params.id}`);
+      let services = await this.http.get(`${this.config.server.url}services?vendor=${params.id}`);
       this.vendor = vendor.content;
+      this.services = services.content;
       routeConfig.navModel.title = this.vendor.name;
     } catch (err) {
       // TODO flash a global error message
