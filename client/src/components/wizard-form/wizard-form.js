29b76b30b2f218e60d5a01b2d6c07af2d5df56c0
Martin Genev <mgenev@gmail.com>
2015-06-24 17:44:11 -0700

    better way to handle active wizard step class

--- client/src/components/wizard-form/wizard-form.js
+++ client/src/components/wizard-form/wizard-form.js
@@ -5,9 +5,6 @@ export class WizardForm {
   @bindable steps = [];
   @bindable model = null;
   activeStep = 0;
-  attached() {
-    this.addActiveClass();
-  }
 
   @computedFrom('activeStep')
   get firstStep() {
@@ -19,24 +16,15 @@ export class WizardForm {
     return this.activeStep === this.steps.length - 1;
   }
 
-  addActiveClass() {
-    $('.wizard-active').removeClass('wizard-active');
-    let selector = `.wizard-step:eq(${this.activeStep})`;
-    $(selector).addClass('wizard-active');
-  }
-
   nextStep() {
     ++this.activeStep;
-    this.addActiveClass();
   }
 
   prevStep() {
     --this.activeStep;
-    this.addActiveClass();
   }
 
   goToStep(step) {
     this.activeStep = step;
-    this.addActiveClass();
   }
 }
